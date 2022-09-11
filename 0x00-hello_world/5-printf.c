122ece49f6439e97fb1eb5f6a6f164883d613407
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2022-09-11 16:07:09 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,8 @@
  *main - print string with printf
  *
  * Description - using the main function
- * The progam will output with proper grammar, but the outcome is a piece of art,
+ * The progam will output with proper grammar,
+ * but the outcome is a piece of art,
  * Return: 0
  */
 int main(void)
