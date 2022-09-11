4dcc281f82d13ff8861e63aacb50c15917013da5
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2022-09-11 15:55:06 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ *main - print string with printf
+ *
+ * Description - using the main function
+ * The progam will output with proper grammar, but the outcome is a piece of art,
+ * Return: 0
+ */
+int main(void)
+{
+char str[] = "with proper grammar"
+"but the outcome is a piece of art,";
+printf("%s\n"str);
+return (0);
+}
