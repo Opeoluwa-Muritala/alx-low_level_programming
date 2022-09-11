fb4754ae83835bfcde39864c1166379bbcee8f77
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2022-09-11 13:22:54 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,12 @@
  * main - print the string in the put function
  *
  * Description: using the main function
- * This program prints "Programming is a multilingual puzzle.
+ * This program prints "Programming is a multilingual puzzle."
+ * Return: 0
  */
 
 int main(void)
 {
-  puts('Programming is a multilingual puzzle')
-  return (0)
-    }
+puts("Programming is a multilingual puzzle")
+return (0)
+}
