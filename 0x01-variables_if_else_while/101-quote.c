bfc6c129a35877cd0e28176b030567c50048c09e
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2022-09-12 10:21:00 -0700

    8

---
+++ 0x01-variables_if_else_while/101-quote.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <sys/syscall.h>
+
+/**
+ * main - print without use of printf or put
+ *
+ * Description -print and that piece of art is useful"
+ * Dora Korpar, 2015-10-19
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+const char msg[] = "Hello, world!\n";
+syscall(SYS_write, 1, msg, sizeof(msg));
+return (1);
+}
