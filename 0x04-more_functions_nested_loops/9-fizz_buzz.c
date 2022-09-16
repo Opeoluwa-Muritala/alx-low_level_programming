5efb0a1cef42700722db68db98a6c432904060ad
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2022-09-16 11:34:08 -0700

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,32 +5,40 @@
 *or fizz or buzz or fizzBuzz
 *
 *
-*
-Return: returns 0
+*Return: returns 0
 */
 
 int main(void)
 {
-int num = 0;
+int num = 1;
 
-while(num++ < 100)
+while (num++ < 100)
 {
 if ((num % 3 == 0) && (num % 5 == 0))
 {
-printf("%d FizzBuzz \n", num);
+printf("FizzBuzz ");
 }
-else if (num % 3 == 0)
+else if ((num % 3) == 0)
 {
-printf("%d Fizz \n", num);
+printf("Fizz ");
 }
-else if (num % 5 == 0)
+else if ((num % 5) == 0)
 {
-printf("%d Buzz");
+if (num != 100)
+{
+printf("Buzz ");
 }
 else
 {
-printf("%d", num);
+printf("Buzz");
+}
+}
+else
+{
+printf("%d ", num);
+}
 }
 printf("\n");
-}
+
+return (0);
 }
