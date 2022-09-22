84699c30c0bd7ee66f4edd0f60d06ec495bcbe6f
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2022-09-22 15:57:29 -0700

    C"

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,28 +4,19 @@
  * _strcat - This uses dest to return a string over src
  *
  *
- * @dest: Ovewriting nul byte at the end
- * @src: Appends string to dest
- *
- * description: This function appends the src string to
- * the dest string, overwriting the terminating null byte
- * (\0) at the end of dest,
- * and then adds a terminating null byte Returns a pointer to
- * the resulting string dest
- *
+ * @dest: First parmeter
+ * @src: Second parameter
+ * @n: Third parameter
+ * description: The _strncat function is similar to the _strcat
+ * function, except that
+ * it will use at most n bytes from src and
+ * src does not need to be null-terminated if it contains
+ * n or more bytes
  * Return: dest
  */
 
 char *_strcat(char *dest, char *src, int n)
 {
-int index = 0, dest_len = 0;
-while (dest[index++])
-{
-dest_len++;
-}
-for (index = 0; src[index] && index < n ; index++)
-{
-dest[dest_len++] = src[index];
-}
+strncat(dest, src, n);
 return (dest);
 }
