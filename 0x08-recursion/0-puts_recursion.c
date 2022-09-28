72a2f03e1cd0a1ddd4308cc6805c67cd4fac5a06
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2022-09-28 03:29:06 -0700

    Recursion task 1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Recurssion
+ * Author - Opeoluwa Muritala
+ *
+ * @s : Store string
+ * Description: print string and a new line
+ *
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+_putchar(s)
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+Assignment on 0x08. C - Recursion
