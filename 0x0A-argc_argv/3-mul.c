f28b0946f9fb4b99c0880d257609fa7b396fd133
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2022-10-03 16:38:56 +0000

    Fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,7 +11,7 @@
 
 int main(int argc, char *argv[])
 {
-int i, j;
+    int i, j;
 
 if (argc == 1 || argc == 2)
 {
@@ -23,10 +23,11 @@ else
 {
     j = 1;
 
-for (i = 1; i < 3; i++)
+    for (i = 1; i < 3; i++)
     j = atoi(argv[i]);
 
-printf("%d\n", j);
+    printf("%d\n", j);
+}
 
 return (0);
 }
