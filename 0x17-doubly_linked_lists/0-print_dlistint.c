91b4fb3b42498eae90f921a764f82339de47495c
Opeoluwa-Muritala <muritalaopeoluwa102gmail.com>
2023-10-12 21:51:23 +0100

    CODE

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,15 +1,16 @@
 #include "lists.h"
+#include <stdio.h>
 
 size_t print_dlistint(const dlistint_t *h)
 {
 	dlistint_t current = *h;
-	int count = 0;
+	size_t count = 0;
 
 	while (current != NULL)
 	{
-		count++;
 		printf("%d\n",current->n);
 		current = current->next;
+		count++;
 	}
 	return (count);
 }
